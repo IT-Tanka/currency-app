91d4d9ab1c36dd22fd59886b2222b49bae14bf5d
it-tanka <79tanka@gmail.com>
2024-08-08 01:07:47 +0300

    removed FormsModule from header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; 
-
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
     standalone: true,
-    imports: [CommonModule, FormsModule] 
+    imports: [CommonModule] 
 })
 export class HeaderComponent {
     title: string = 'Currency-App';
